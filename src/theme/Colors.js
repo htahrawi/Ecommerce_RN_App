db3c1ba5f2fa642ea894d0d55581769dd0e95435
htahrawi <h.tahrawi01@gmail.com>
2022-11-27 16:08:49 +0200

    make the structure for the app relates #2

---
+++ src/theme/Colors.js
@@ -0,0 +1,22 @@
+const colors = {
+  primary: '#FD5606',
+  secondry: '#FE8E59',
+//   background: '#',
+  white: '#fff',
+  black: '#000'
+};
+
+const gray = {
+  50: '#F9FAFB',
+  100: '#F3F4F6',
+  200: '#E5E7EB',
+  300: '#D1D5DB',
+  400: '#9CA3AF',
+  500: '#6B7280',
+  600: '#4B5563',
+  700: '#374151',
+  800: '#1F2937',
+  900: '#111827',
+};
+// color: #8C8FA5;
+export default {...colors, gray};
