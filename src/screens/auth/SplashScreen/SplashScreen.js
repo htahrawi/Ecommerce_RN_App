8dc193c6093711effe433235ec7f1b6b36e77c4b
htahrawi <h.tahrawi01@gmail.com>
2022-12-04 12:01:07 +0200

    create splash screen relates #3

--- src/screens/auth/SplashScreen/SplashScreen.js
+++ src/screens/auth/SplashScreen/SplashScreen.js
@@ -1,15 +1,65 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Image,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import React from 'react';
+import Colors from '../../../theme/Colors';
+import {CustomButton} from '../../../components';
 
-
-const SplashScreen = () => {
+const SplashScreen = props => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
+    <View style={styles.container}>
+      <StatusBar backgroundColor={Colors.primary} />
+      <View style={{alignItems: 'center'}}>
+        <View style={styles.vectorContainer}>
+          <Image
+            source={require('../../../assets/images/Vector.png')}
+            style={styles.vector}
+            resizeMode="stretch"
+          />
+        </View>
+        <Text style={styles.txt}>Shopping App</Text>
+      </View>
+      <CustomButton
+        onPress={() => {
+          props.navigation.navigate('signIn');
+        }}
+        title="Get Started"
+        type="SECONDARY"
+        />
     </View>
-  )
-}
+  );
+};
 
 export default SplashScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Colors.primary,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%',
+  },
+  vectorContainer: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'white',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  vector: {
+    width: 50,
+    height: 50,
+  },
+  txt: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+});
